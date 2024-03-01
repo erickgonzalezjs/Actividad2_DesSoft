c48906ab382c9c64777372927f96950f07c5b804
erickgonzalezjs <145169672+erickgonzalezjs@users.noreply.github.com>
2024-03-01 18:44:28 -0500

    Update main.py
    Importacion de curso y estudiantes y llamarlos

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # main.py
-
+import curso
+import estudiantes
 def main():
     # Obtener información del curso
-    curso_mostrar_informacion_curso()
+    curso.curso_mostrar_informacion_curso()
 
     # Obtener información de los estudiantes
-    estudiantes_mostrar_estadisticas_estudiantes()
+    estudiantes.estudiantes_mostrar_estadisticas_estudiantes()
 
 if __name__ == "__main__":
     main()
